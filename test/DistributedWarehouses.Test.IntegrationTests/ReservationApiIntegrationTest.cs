97262ccb82ceb2c16d1f70fe6be8aa29cff29b7e
AugustasCepas <augustas.cepas@gmail.com>
2021-11-01 00:37:29 +0200

    Unit & Integration tests example

---
+++ test/DistributedWarehouses.Test.IntegrationTests/ReservationApiIntegrationTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DistributedWarehouses.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+
+namespace DistributedWarehouses.Test.IntegrationTests
+{
+    public class ReservationApiIntegrationTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ReservationApiIntegrationTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/v1/Invoices")]
+        [InlineData("/v1/Items")]
+        [InlineData("/v1/Reservations")]
+        [InlineData("/v1/Warehouses")]
+        public async Task GetHttpRequest(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ test/DistributedWarehouses.Test.IntegrationTests/TestClientProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using DistributedWarehouses.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace DistributedWarehouses.Test.IntegrationTests
+{
+    public class TestClientProvider
+    {
+        public HttpClient Client { get; private set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
